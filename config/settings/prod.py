24c24b90981894d459a58754313792ff7db2d0ba
joshdels <assistantgisjosh@gmail.com>
2026-09-17 19:46:34 +0800

    feat: config and docker

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,13 +6,33 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 DEBUG = False
 
-
 ALLOWED_HOSTS = [
     host.strip() for host in os.environ["ALLOWED_HOSTS"].split(",") if host.strip()
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    origin.strip()
+    for origin in os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
+    if origin.strip()
+]
 
-# PostgreSQL
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+USE_X_FORWARDED_HOST = True
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -25,77 +45,40 @@ DATABASES = {
 }
 
 
-# Backblaze B2 / S3
-AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
-
-AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
-
-AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
-
-AWS_S3_ENDPOINT_URL = os.environ["AWS_S3_ENDPOINT_URL"]
-
-AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
-
-AWS_S3_SIGNATURE_VERSION = "s3v4"
-
-AWS_S3_FILE_OVERWRITE = False
-
-AWS_DEFAULT_ACL = None
-
-AWS_QUERYSTRING_AUTH = False
-
-
-# Static files
 STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3.S3Storage",
         "OPTIONS": {
-            "location": "media",
+            "access_key": os.environ["B2_APPLICATION_KEY_ID"],
+            "secret_key": os.environ["B2_APPLICATION_KEY"],
+            "bucket_name": os.environ["B2_BUCKET_NAME"],
+            "endpoint_url": os.environ["B2_ENDPOINT_URL"],
+            "region_name": os.environ["B2_REGION"],
+            "default_acl": None,
+            "querystring_auth": False,
         },
     },
     "staticfiles": {
-        "BACKEND": "storages.backends.s3.S3Storage",
-        "OPTIONS": {
-            "location": "static",
-        },
+        "BACKEND": ("whitenoise.storage.CompressedManifestStaticFilesStorage"),
     },
 }
 
 
-STATIC_URL = (
-    f"{os.environ['AWS_S3_ENDPOINT_URL']}/"
-    f"{os.environ['AWS_STORAGE_BUCKET_NAME']}/static/"
-)
+STATIC_URL = "/static/"
 
-MEDIA_URL = (
-    f"{os.environ['AWS_S3_ENDPOINT_URL']}/"
-    f"{os.environ['AWS_STORAGE_BUCKET_NAME']}/media/"
-)
+MEDIA_URL = f"{os.environ['B2_ENDPOINT_URL']}/" f"{os.environ['B2_BUCKET_NAME']}/media/"
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-# Wagtail
+EMAIL_HOST = os.environ["BREVO_HOST"]
+EMAIL_PORT = int(os.environ.get("BREVO_PORT", "587"))
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ["BREVO_SMTP_LOGIN"]
+EMAIL_HOST_PASSWORD = os.environ["BREVO_SMTP_PASSWORD"]
+
+DEFAULT_FROM_EMAIL = os.environ["WEBSITE_EMAIL"]
+SERVER_EMAIL = os.environ["WEBSITE_EMAIL"]
+
 WAGTAILADMIN_BASE_URL = os.environ["WAGTAILADMIN_BASE_URL"]
-
-
-# CSRF
-CSRF_TRUSTED_ORIGINS = [
-    origin.strip()
-    for origin in os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
-    if origin.strip()
-]
-
-
-# Security
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
-SECURE_SSL_REDIRECT = True
-
-SESSION_COOKIE_SECURE = True
-
-CSRF_COOKIE_SECURE = True
-
-SECURE_HSTS_SECONDS = 31536000
-
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-
-SECURE_HSTS_PRELOAD = True
